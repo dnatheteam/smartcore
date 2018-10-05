c18d894d455b04d3c42ce7c361f4b251979ab88c
Airat Sirazutdinov <siraz@yandex.ru>
2018-10-05 12:03:41 +0300

    Check email presence request updated

--- lib/smartcore/requests/information_requests/check_email_presence_request.rb
+++ lib/smartcore/requests/information_requests/check_email_presence_request.rb
@@ -5,9 +5,9 @@ module Smartcore
     def execute
       response = execute_request
       if response.status == success_status
-        true
+        { status: true, api_id: response.api_id }
       elsif response.status == 404
-        false
+        { status: false }
       else
         process_error(response)
       end

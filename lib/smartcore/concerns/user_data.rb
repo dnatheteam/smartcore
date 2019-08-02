0b2d456bbf4897f276543668a72af7c609fa2c6a
gen <gen1321@mail.ru>
2019-08-02 15:16:38 +0400

    update for reg source

--- lib/smartcore/concerns/user_data.rb
+++ lib/smartcore/concerns/user_data.rb
@@ -1,47 +1,48 @@
 module Smartcore
   module UserData
     include Virtus.module
-    attribute :first_name,          String
-    attribute :last_name,           String
-    attribute :patronymic,          String
+    attribute :first_name, String
+    attribute :last_name, String
+    attribute :patronymic, String
 
-    attribute :login,               String
-    attribute :email,               String
-    attribute :phone_number,        String
-    attribute :union_email,         String
+    attribute :login, String
+    attribute :email, String
+    attribute :phone_number, String
+    attribute :union_email, String
 
-    attribute :gender,              String # male|female
-    attribute :birthday,            Date
+    attribute :gender, String # male|female
+    attribute :birthday, Date
 
-    attribute :verification_state,    Symbol
+    attribute :verification_state, Symbol
     attribute :verification_activiti, Smartcore::Models::UserActivity
 
-    attribute :accept_term_at,      DateTime
-    attribute :avatar_url,          String
+    attribute :accept_term_at, DateTime
+    attribute :avatar_url, String
 
-    attribute :document_uploaded,   Boolean
-    attribute :email_confirmed,     Boolean
-    attribute :valid,               Boolean
-    attribute :subscribed,          Boolean
+    attribute :document_uploaded, Boolean
+    attribute :email_confirmed, Boolean
+    attribute :valid, Boolean
+    attribute :subscribed, Boolean
+    attribute :reg_source, String
 
-    attribute :preferred_cigarette_brand,     Smartcore::Models::Brand
-    attribute :preferred_cigarette_format,    Smartcore::Models::CigaretteFormat
+    attribute :preferred_cigarette_brand, Smartcore::Models::Brand
+    attribute :preferred_cigarette_format, Smartcore::Models::CigaretteFormat
 
-    attribute :secondary_cigarette_brand,     Smartcore::Models::Brand
-    attribute :secondary_cigarette_format,    Smartcore::Models::CigaretteFormat
+    attribute :secondary_cigarette_brand, Smartcore::Models::Brand
+    attribute :secondary_cigarette_format, Smartcore::Models::CigaretteFormat
 
-    attribute :country,             Smartcore::Models::Country
-    attribute :region,              Smartcore::Models::Region
-    attribute :city,                Smartcore::Models::City
+    attribute :country, Smartcore::Models::Country
+    attribute :region, Smartcore::Models::Region
+    attribute :city, Smartcore::Models::City
 
-    attribute :street,              String
-    attribute :postcode,            String
-    attribute :marks,               Hash
+    attribute :street, String
+    attribute :postcode, String
+    attribute :marks, Hash
 
-    attribute :oauth_credentials,   Array[Smartcore::Models::OAuthCredential]
+    attribute :oauth_credentials, Array[Smartcore::Models::OAuthCredential]
 
-    attribute :client,              String
-    attribute :created_at,          DateTime
-    attribute :email_confirmation_token,  String
+    attribute :client, String
+    attribute :created_at, DateTime
+    attribute :email_confirmation_token, String
   end
 end
--- lib/smartcore/version.rb
+++ lib/smartcore/version.rb
@@ -1,10 +1,8 @@
 module Smartcore
-
   MAJOR = 1
-  MINOR = 7
-  TINY  = 4
-  PRE   = nil
+  MINOR = 8
+  TINY = 4
+  PRE = nil
 
   VERSION = [MAJOR, MINOR, TINY, PRE].compact.join('.')
-
 end
